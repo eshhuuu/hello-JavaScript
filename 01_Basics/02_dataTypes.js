3e55158627e1e71b0b8c35ff18485f45e2390bc3
eshuu <ayeshhh101@gmail.com>
2026-02-08 07:49:35 +0500

    DataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,18 @@
+//Data Types
+
+"use strict"; //treats all JS code as newer version
+
+
+
+//Primitive Data types
+let name = "Ayesha"  //string
+let age = 18   //number
+let isLoggedIn = false  //boolean
+let state; //undefined (no value assigned)
+let city = null; //Null (empty value, standalone value)
+//symbol (to define uniqueness)
+//bigInt
+
+//Object
+
+console.log(typeof "Ayesha");
